696cdf4bba25fc7f28b4d934ea4d5a61cca600d1
d-ivashchuk <dimaiv@me.com>
2023-01-21 16:33:30 +0100

    set up lost-pixel platform integration

---
+++ lostpixel.config.ts
@@ -0,0 +1,10 @@
+import { CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+  storybookShots: {
+    storybookUrl: "./storybook-static",
+  },
+
+  lostPixelProjectId: "cld63hw4e0gx1l50v5zv9gm20",
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
